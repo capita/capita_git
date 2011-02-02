5bfb0aa3806023945f1d40cf3441dd64a9ed2272
Sebastian Georgi <sgeorgi@capita.de>
2011-02-02 18:25:16 +0100

    Load path and require statements updated

--- lib/capita_git.rb
+++ lib/capita_git.rb
@@ -1,5 +1,9 @@
+$:.unshift File.expand_path(__FILE__)
+require 'rubygems'
+
 module CapitaGit
   autoload :UI, 'capita_git/ui'
+  autoload :CLI,'capita_git/cli'
   autoload :Repository, 'capita_git/repository'
 
   class << self
